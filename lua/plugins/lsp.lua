666f338884bfc15ce1609de8641c6c2a786a54f6
JonLD <jlloyddavies@yahoo.com>
2025-07-14 03:22:21 +0100

    Add clangd lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,23 @@
 return {
-    -- add pyright to lspconfig
     {
         "neovim/nvim-lspconfig",
         ---@class PluginLspOpts
         opts = {
+            servers = {
+                clangd = {
+                    root_dir = function(fname)
+                        return require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+                            fname
+                        ) or vim.fs.dirname(vim.fs.find('.git', { path = fname, upward = true })[1])
+                    end,
+                },
+            },
             diagnostics = {
                 underline = true,
                 update_in_insert = false,
                 virtual_text = false,
             },
 
-            -- pyright will be automatically installed with mason and loaded with lspconfig
-            -- pyright = {},
-            -- },
         },
     },
     { "nushell/tree-sitter-nu", event = "FileType", filetyoe = "nu", build = ":TSUpdate nu" },
