290853ef80fec2f49a70ef8eca293f6d17866352
JonLD <jlloyddavies@yahoo.com>
2025-07-25 15:02:04 +0100

    AI config

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Hide terminal buffers from bufferline
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.buflisted = false
+  end,
+  desc = "Hide terminal buffers from bufferline"
+})
