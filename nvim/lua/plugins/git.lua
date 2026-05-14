00f1d60b26fadfb7b359a1ec5bf86720e35efc03
JonLD <jlloyddavies@yahoo.com>
2026-05-14 13:44:59 +0100

    Add diffview.nvim and gitlab.nvim for MR review
    - diffview: toggle bindings for working tree (<leader>gn) and branch vs main (<leader>gm)
    - gitlab.nvim: MR review with discussions, comments, approvals under <leader>M
    - Monkey-patch server.start to use list-form jobstart, bypassing nushell on Windows
    - Use dlyongemallo/diffview.nvim fork (maintained)
    - Auto-detect git root and convert unix-style path for config_path

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -7,66 +7,102 @@ require("which-key").add({
 })
 
 return {
-    "lewis6991/gitsigns.nvim",
-    version = "v1.0.0",
-    lazy = true,
-    keys = {
-        {
-            "<leader>gj",
-            function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "]c", bang = true })
-                else
-                    require("gitsigns").nav_hunk("next", {navigation_message = false})
-                end
-            end,
-            desc = "Next Hunk",
+    {
+        "dlyongemallo/diffview.nvim",
+        lazy = true,
+        keys = {
+            {
+                "<leader>gn",
+                function()
+                    local lib = require("diffview.lib")
+                    local view = lib.get_current_view()
+                    if view then
+                        vim.cmd("DiffviewClose")
+                    else
+                        vim.cmd("DiffviewOpen")
+                    end
+                end,
+                desc = "Toggle Diffview",
+            },
+            {
+                "<leader>gm",
+                function()
+                    local lib = require("diffview.lib")
+                    local view = lib.get_current_view()
+                    if view then
+                        vim.cmd("DiffviewClose")
+                    else
+                        vim.cmd("DiffviewOpen origin/HEAD...HEAD")
+                    end
+                end,
+                desc = "Toggle Branch Diff vs Main",
+            },
+            { "<leader>gh", "<cmd>DiffviewFileHistory %<CR>", desc = "File History" },
+            { "<leader>gH", "<cmd>DiffviewFileHistory<CR>", desc = "Repo History" },
         },
-        {
-            "<leader>gk",
-            function()
-                if vim.wo.diff then
-                    vim.cmd.normal({ "[c", bang = true })
-                else
-                    require("gitsigns").nav_hunk("prev", {navigation_message = false})
-                end
-            end,
-            desc = "Prev Hunk",
-        },
-        {
-            "<leader>gJ",
-            function()
-                require("gitsigns").nav_hunk("last", {navigation_message = false})
-            end,
-            desc = "Last Hunk",
-        },
-        {
-            "<leader>gK",
-            function()
-                require("gitsigns").nav_hunk("first", {navigation_message = false})
-            end,
-            desc = "First Hunk",
-        },
-        { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", desc = "Stage Hunk" },
-        { "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset Hunk" },
-        { "<leader>gS", "<cmd>Gitsigns stage_buffer<CR>", desc = "Stage Buffer" },
-        { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "Undo Stage Hunk" },
-        { "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", desc = "Reset Buffer" },
-        { "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", desc = "Preview hunk inline" },
-        { "<leader>gP", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview hunk" },
-        {
-            "<leader>ghb",
-            function()
-                require("gitsigns").blame_line({ full = true })
-            end,
-            desc = "Blame Line",
-        },
-        {
-            "<leader>ghB",
-            function()
-                require("gitsigns").blame()
-            end,
-            desc = "Blame Buffer",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        version = "v1.0.0",
+        lazy = true,
+        keys = {
+            {
+                "<leader>gj",
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "]c", bang = true })
+                    else
+                        require("gitsigns").nav_hunk("next", {navigation_message = false})
+                    end
+                end,
+                desc = "Next Hunk",
+            },
+            {
+                "<leader>gk",
+                function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "[c", bang = true })
+                    else
+                        require("gitsigns").nav_hunk("prev", {navigation_message = false})
+                    end
+                end,
+                desc = "Prev Hunk",
+            },
+            {
+                "<leader>gJ",
+                function()
+                    require("gitsigns").nav_hunk("last", {navigation_message = false})
+                end,
+                desc = "Last Hunk",
+            },
+            {
+                "<leader>gK",
+                function()
+                    require("gitsigns").nav_hunk("first", {navigation_message = false})
+                end,
+                desc = "First Hunk",
+            },
+            { "<leader>gs", "<cmd>Gitsigns stage_hunk<CR>", desc = "Stage Hunk" },
+            { "<leader>gr", "<cmd>Gitsigns reset_hunk<CR>", desc = "Reset Hunk" },
+            { "<leader>gS", "<cmd>Gitsigns stage_buffer<CR>", desc = "Stage Buffer" },
+            { "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "Undo Stage Hunk" },
+            { "<leader>gR", "<cmd>Gitsigns reset_buffer<CR>", desc = "Reset Buffer" },
+            { "<leader>gp", "<cmd>Gitsigns preview_hunk_inline<CR>", desc = "Preview hunk inline" },
+            { "<leader>gP", "<cmd>Gitsigns preview_hunk<CR>", desc = "Preview hunk" },
+            {
+                "<leader>ghb",
+                function()
+                    require("gitsigns").blame_line({ full = true })
+                end,
+                desc = "Blame Line",
+            },
+            {
+                "<leader>ghB",
+                function()
+                    require("gitsigns").blame()
+                end,
+                desc = "Blame Buffer",
+            },
         },
     },
 }
--- nvim/lua/plugins/gitlab.lua
+++ nvim/lua/plugins/gitlab.lua
@@ -1,34 +1,91 @@
 return {
-  'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
-  build = 'npm install',
-  -- Activate when a file is created/opened
-  event = { 'BufReadPre', 'BufNewFile' },
-  -- Activate when a supported filetype is open
-  ft = { 'c', 'cpp', 'go', 'python', 'rust', 'lua', 'typescript' },
-  cond = function()
-    -- Only activate if token is present in environment variable.
-    -- Remove this line to use the interactive workflow.
-    return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
-  end,
-  opts = {
     {
-      statusline = {
-        enabled = true
-      },
-      code_suggestions = {
-        enabled = true,
-        -- debounce_ms = 100, -- default is usually higher
-        -- For the full list of default languages, see the 'auto_filetypes' array in
-        -- https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim/-/blob/main/lua/gitlab/config/defaults.lua
-        auto_filetypes = { 'c', 'cpp', 'go', 'python', 'rust', 'lua', 'typescript' }, -- Default is { 'ruby' }
-        ghost_text = {
-          enabled = true, -- ghost text is an experimental feature
-          toggle_enabled = "<C-h>",
-          accept_suggestion = "<C-l>",
-          clear_suggestions = "<C-f>",
-          stream = true,
+        'https://gitlab.com/gitlab-org/editor-extensions/gitlab.vim.git',
+        enabled = false,
+        build = 'npm install',
+        event = { 'BufReadPre', 'BufNewFile' },
+        ft = { 'c', 'cpp', 'go', 'python', 'rust', 'lua', 'typescript' },
+        cond = function()
+            return vim.env.GITLAB_TOKEN ~= nil and vim.env.GITLAB_TOKEN ~= ''
+        end,
+        opts = {
+            {
+                statusline = {
+                    enabled = true
+                },
+                code_suggestions = {
+                    enabled = true,
+                    auto_filetypes = { 'c', 'cpp', 'go', 'python', 'rust', 'lua', 'typescript' },
+                    ghost_text = {
+                        enabled = true,
+                        toggle_enabled = "<C-h>",
+                        accept_suggestion = "<C-l>",
+                        clear_suggestions = "<C-f>",
+                        stream = true,
+                    },
+                }
+            }
         },
-      }
-    }
-  },
+    },
+    {
+        "JonLD/gitlab.nvim",
+        dev = true,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "dlyongemallo/diffview.nvim",
+            "stevearc/dressing.nvim",
+        },
+        build = "go build -o bin/server .",
+        lazy = true,
+        keys = {
+            {
+                "<leader>Mo",
+                function()
+                    local lib = require("diffview.lib")
+                    if lib.get_current_view() then
+                        require("gitlab").close_review()
+                    else
+                        require("gitlab").review()
+                    end
+                end,
+                desc = "Toggle MR Review",
+            },
+            { "<leader>Ms", "<cmd>lua require('gitlab').summary()<CR>", desc = "MR Summary" },
+            { "<leader>Mc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "Create Comment" },
+            { "<leader>Mn", "<cmd>lua require('gitlab').move_to_discussion_tree_from_diagnostic()<CR>", desc = "Jump to Discussion" },
+            { "<leader>Ml", "<cmd>lua require('gitlab').list_discussions()<CR>", desc = "List Discussions" },
+            { "<leader>Mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "MR Pipeline" },
+            { "<leader>Mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR URL" },
+        },
+        config = function()
+            local root = vim.fn.trim(vim.fn.system({ "git", "rev-parse", "--show-toplevel" }))
+            root = root:gsub("^/(%a)/", "%1:/"):gsub("/", "\\")
+            -- Patch server.start to use list-form jobstart, bypassing nushell on Windows
+            local server = require("gitlab.server")
+            local orig_start = server.start
+            server.start = function(callback)
+                local orig_jobstart = vim.fn.jobstart
+                vim.fn.jobstart = function(cmd, opts)
+                    if type(cmd) == "string" and cmd:find("gitlab.nvim", 1, true) then
+                        local binary = cmd:match('^"([^"]+)"')
+                        local json = cmd:match('^"[^"]*" "(.*)"$')
+                        if binary and json then
+                            json = json:gsub('\\"', '"')
+                            vim.fn.jobstart = orig_jobstart
+                            return orig_jobstart({ binary, json }, opts)
+                        end
+                    end
+                    return orig_jobstart(cmd, opts)
+                end
+                return orig_start(callback)
+            end
+            require("gitlab").setup({
+                config_path = root,
+                discussion_signs = {
+                    virtual_text = true,
+                },
+            })
+        end,
+    },
 }
