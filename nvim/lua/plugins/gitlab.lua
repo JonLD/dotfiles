621d466c4c4d0366cdb60cc6277c3e95a78bb641
JonLD <jlloyddavies@yahoo.com>
2026-05-14 13:56:36 +0100

    Remove workaround from gitlab.nvim config

--- nvim/lua/plugins/gitlab.lua
+++ nvim/lua/plugins/gitlab.lua
@@ -58,34 +58,6 @@ return {
             { "<leader>Mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "MR Pipeline" },
             { "<leader>Mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR URL" },
         },
-        config = function()
-            local root = vim.fn.trim(vim.fn.system({ "git", "rev-parse", "--show-toplevel" }))
-            root = root:gsub("^/(%a)/", "%1:/"):gsub("/", "\\")
-            -- Patch server.start to use list-form jobstart, bypassing nushell on Windows
-            local server = require("gitlab.server")
-            local orig_start = server.start
-            server.start = function(callback)
-                local orig_jobstart = vim.fn.jobstart
-                vim.fn.jobstart = function(cmd, opts)
-                    if type(cmd) == "string" and cmd:find("gitlab.nvim", 1, true) then
-                        local binary = cmd:match('^"([^"]+)"')
-                        local json = cmd:match('^"[^"]*" "(.*)"$')
-                        if binary and json then
-                            json = json:gsub('\\"', '"')
-                            vim.fn.jobstart = orig_jobstart
-                            return orig_jobstart({ binary, json }, opts)
-                        end
-                    end
-                    return orig_jobstart(cmd, opts)
-                end
-                return orig_start(callback)
-            end
-            require("gitlab").setup({
-                config_path = root,
-                discussion_signs = {
-                    virtual_text = true,
-                },
-            })
-        end,
+        opts = {},
     },
 }
