5e9c52398867768840a3856fc484e33f5276506d
JonLD <jlloyddavies@yahoo.com>
2026-05-15 12:34:09 +0100

    Rebind gitlab.nvim, highlight group changes for diff view

--- nvim/lua/plugins/gitlab.lua
+++ nvim/lua/plugins/gitlab.lua
@@ -28,19 +28,34 @@ return {
         },
     },
     {
+        "dlyongemallo/diffview.nvim",
+        opts = {
+            enhanced_diff_hl = true,
+            view = {
+                default = { layout = "diff2_horizontal" },
+            },
+        },
+        config = function(_, opts)
+            require("diffview").setup(opts)
+            -- Filler lines on the opposite side of an add/delete: blend into background
+            vim.api.nvim_set_hl(0, "DiffviewDiffDeleteDim", { link = "Normal" })
+            vim.api.nvim_set_hl(0, "DiffviewDiffAddDim", { link = "Normal" })
+            vim.opt.fillchars:append({ diff = " " })
+        end,
+    },
+    {
         "JonLD/gitlab.nvim",
         dev = true,
         dependencies = {
             "MunifTanjim/nui.nvim",
             "nvim-lua/plenary.nvim",
             "dlyongemallo/diffview.nvim",
-            "stevearc/dressing.nvim",
         },
         build = "go build -o bin/server .",
         lazy = true,
         keys = {
             {
-                "<leader>Mo",
+                "<leader>mo",
                 function()
                     local lib = require("diffview.lib")
                     if lib.get_current_view() then
@@ -51,13 +66,28 @@ return {
                 end,
                 desc = "Toggle MR Review",
             },
-            { "<leader>Ms", "<cmd>lua require('gitlab').summary()<CR>", desc = "MR Summary" },
-            { "<leader>Mc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "Create Comment" },
-            { "<leader>Mn", "<cmd>lua require('gitlab').move_to_discussion_tree_from_diagnostic()<CR>", desc = "Jump to Discussion" },
-            { "<leader>Ml", "<cmd>lua require('gitlab').list_discussions()<CR>", desc = "List Discussions" },
-            { "<leader>Mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "MR Pipeline" },
-            { "<leader>Mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR URL" },
+            { "<leader>ms", "<cmd>lua require('gitlab').summary()<CR>", desc = "MR Summary" },
+            { "<leader>mc", "<cmd>lua require('gitlab').create_comment()<CR>", desc = "Create Comment" },
+            { "<leader>mn", "<cmd>lua require('gitlab').move_to_discussion_tree_from_diagnostic()<CR>", desc = "Jump to Discussion" },
+            { "<leader>ml", "<cmd>lua require('gitlab').list_discussions()<CR>", desc = "List Discussions" },
+            { "<leader>mp", "<cmd>lua require('gitlab').pipeline()<CR>", desc = "MR Pipeline" },
+            { "<leader>mu", "<cmd>lua require('gitlab').copy_mr_url()<CR>", desc = "Copy MR URL" },
+            { "<leader>mm", "<cmd>lua require('gitlab').choose_merge_request()<CR>", desc = "Choose MR" },
+            { "]g", function() require("gitlab").next_discussion() end, desc = "Next MR comment" },
+            { "[g", function() require("gitlab").prev_discussion() end, desc = "Prev MR comment" },
         },
-        opts = {},
+        opts = {
+            discussion_tree = {
+                auto_open = false,
+            },
+            discussion_signs = {
+                icons = {
+                    comment = "💬",
+                    range = "│",
+                    resolved = " ",
+                    resolved_range = "│",
+                },
+            },
+        },
     },
 }
